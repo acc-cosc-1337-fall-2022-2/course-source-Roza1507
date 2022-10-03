4acdf92a5e5f0a129bf7bce1aae0b752e8cad7b1
Roza1507 <roza.arinova@g.austincc.edu>
2022-10-03 22:19:35 +0000

    homework1

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -3,10 +3,17 @@
 //example
 int add_numbers(int num1, int num2)
 {
-	return num1 + num2;
+	return num1 * num2;
 }
 
 //write function code here
+int multiply_numbers(int num1) 
+{
+	int num2=5;
+	return num1*num2;
+
+}
+
 
 
 
--- src/homework/01_data_types/data_types.h
+++ src/homework/01_data_types/data_types.h
@@ -1,5 +1,5 @@
 ﻿//example
-int add_numbers(int num1, int num2);
+int multiply_numbers(int num1);
 
 //write function prototype here
 
